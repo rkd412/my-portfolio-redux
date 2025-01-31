602ca0fd0091100a2304ac99adea92bdcf117bcc
Kingslow <rkdavis412@gmail.com>
2025-01-31 09:57:16 -0500

    Geez these Vercel deployments are sensitive

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function Custom404() {
   return (
     <h1 className="mt-24 mb-4 text-4xl font-bold leading-none tracking-tight md:text-5xl lg:text-6xl">
-      Hey! You&apos;re not suppose to be here. That&apos;s alright though. <a href="/">Here, let&apos;s
-      head back home</a>.
+      Hey! You&apos;re not suppose to be here. That&apos;s alright though.{" "}
+      <Link href="/">Here, let&apos;s head back home</Link>.
     </h1>
   );
 }
