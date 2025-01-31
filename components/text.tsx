4175d2214eac6f565c64286d58df8fbeaebe152d
Kingslow <rkdavis412@gmail.com>
2025-01-31 11:28:06 -0500

    Rewriting content again

--- components/text.tsx
+++ components/text.tsx
@@ -2,10 +2,11 @@ export default function Text() {
   return (
     <>
       <h1 className="mt-16 mb-8 text-4xl tracking-tight md:text-5xl lg:text-6xl">
-        Hi, I'm Rob — a web developer passionate about crafting great digital experiences.
+        Hi, I'm Rob — a web developer focus on crafting great digital
+        experiences.
       </h1>
       <p className="mb-4 text-2xl leading-loose">
-        I've always loved technology, and right now, I'm putting that passion to work for the{" "}
+        I've always loved technology, and right now, I'm helping the{" "}
         <a
           aria-label="City of Pittsburgh"
           href="https://pittsburghpa.gov/"
@@ -13,8 +14,9 @@ export default function Text() {
           rel="noopener noreferrer"
         >
           City of Pittsburgh
-        </a>
-        . Before that, I helped build successful e-commerce platforms at{" "}
+        </a>{" "}
+        improve and modernize their website. Before that, I helped build
+        successful e-commerce platforms at{" "}
         <a
           aria-label="Online Stores, LLC"
           href="https://www.onlinestores.com/"
@@ -23,10 +25,7 @@ export default function Text() {
         >
           Online Stores, LLC
         </a>
-        .
-      </p>
-      <p className="mb-8 text-2xl leading-loose">
-        If you're curious about my work, feel free to{" "}
+        . If you're curious about my work, feel free to{" "}
         <a aria-label="my email address" href="mailto:rob@robertkdavis.com">
           reach out via email
         </a>
@@ -34,7 +33,8 @@ export default function Text() {
         <a href="/resume.pdf" download="resume">
           download my resume
         </a>
-        . You’ll also find more ways to connect in the footer below. Thanks for stopping by!
+        . You’ll also find more ways to connect in the footer below. Thanks for
+        stopping by.
       </p>
     </>
   );
