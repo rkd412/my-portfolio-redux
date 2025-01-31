8141389f2d145082cdbccf0e1a199c77356b89d1
Kingslow <rkdavis412@gmail.com>
2025-01-31 09:54:00 -0500

    Fixing eslint errors for build

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function Custom404() {
   return (
     <h1 className="mt-24 mb-4 text-4xl font-bold leading-none tracking-tight md:text-5xl lg:text-6xl">
-      Hey! You're not suppose to be here. That's alright though. <a href="/">Here, let's
+      Hey! You&apos;re not suppose to be here. That&apos;s alright though. <a href="/">Here, let&apos;s
       head back home</a>.
     </h1>
   );
